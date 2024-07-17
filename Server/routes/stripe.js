b43c46fab4f309dcda35cddf0b20a978aa08f2c2
danielkmetz <60449135+danielkmetz@users.noreply.github.com>
2024-07-17 10:47:53 -0500

    fixed stripe charging

--- Server/routes/stripe.js
+++ Server/routes/stripe.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY_TEST);
 
 router.post('/', async (req, res) => {
-  const {email, buyIn} = req.body
+  const {email, buyIn} = req.body;
+
+  const stripeBuyIn = buyIn * 100;
+
+  console.log(buyIn);
   try {
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: buyIn,
+      amount: stripeBuyIn,
       currency: 'usd',
     });
   
